dc228e878ed69be7507c3173ba228278945cd734
shehan kaushalya <shehankaushalyadurage@gmail.com>
2025-11-21 16:32:49 +0530

    auth

--- src/main/java/com/todoapp/todo/controller/AuthController.java
+++ src/main/java/com/todoapp/todo/controller/AuthController.java
@@ -1,6 +1,15 @@
 package com.todoapp.todo.controller;
 
+import com.todoapp.todo.domain.dto.AuthorDto;
+import com.todoapp.todo.domain.dto.LoginRequest;
+import com.todoapp.todo.domain.dto.RegisterDto;
+import com.todoapp.todo.service.AuthenticationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthorDto> login(@Valid @RequestBody LoginRequest loginRequest) {
+        AuthorDto authenticate = authenticationService.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+        return ResponseEntity.ok(authenticate);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@Valid @RequestBody RegisterDto registerDto) {
+        authenticationService.RegisterUser(registerDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
