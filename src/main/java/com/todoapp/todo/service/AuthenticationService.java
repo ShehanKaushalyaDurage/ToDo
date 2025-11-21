7d0c6619cec0c7a1efd9192342771876479ec17b
shehan kaushalya <shehankaushalyadurage@gmail.com>
2025-11-21 16:16:53 +0530

    auth

--- src/main/java/com/todoapp/todo/service/AuthenticationService.java
+++ src/main/java/com/todoapp/todo/service/AuthenticationService.java
@@ -1,4 +1,9 @@
 package com.todoapp.todo.service;
 
+import com.todoapp.todo.domain.dto.AuthorDto;
+import com.todoapp.todo.domain.dto.RegisterDto;
+
 public interface AuthenticationService {
+    AuthorDto authenticate(String email, String password);
+    void RegisterUser(RegisterDto registerDto);
 }
--- src/main/java/com/todoapp/todo/service/impl/AuthrnticationServiceImpl.java
+++ src/main/java/com/todoapp/todo/service/impl/AuthrnticationServiceImpl.java
@@ -1,4 +1,50 @@
 package com.todoapp.todo.service.impl;
 
-public class AuthrnticationServiceImpl {
+import com.todoapp.todo.domain.dto.AuthorDto;
+import com.todoapp.todo.domain.dto.RegisterDto;
+import com.todoapp.todo.domain.entity.User;
+import com.todoapp.todo.repo.UserRepo;
+import com.todoapp.todo.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthrnticationServiceImpl implements AuthenticationService {
+
+    private final UserRepo userRepo;
+
+    @Override
+    public AuthorDto authenticate(String email, String password) {
+        Optional<User> user = userRepo.findByEmail(email);
+
+        if (user.isPresent()) {
+            if (user.get().getPassword().equals(password)) {
+                return new AuthorDto(user.get().getId(), user.get().getEmail());
+            }else {
+                throw new IllegalArgumentException("Wrong password");
+            }
+        }else  {
+            throw new IllegalArgumentException("Wrong email");
+        }
+    }
+
+    @Transactional
+    @Override
+    public void RegisterUser(RegisterDto registerDto) {
+
+        if (userRepo.findByEmail(registerDto.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("Email already exists");
+        }else {
+            User user = new User();
+            user.setName(registerDto.getName());
+            user.setEmail(registerDto.getEmail());
+            user.setPassword(registerDto.getPassword());
+            userRepo.save(user);
+        }
+
+    }
 }
